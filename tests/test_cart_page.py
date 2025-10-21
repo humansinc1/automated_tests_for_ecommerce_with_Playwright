688f3b82b414c255ad5a37b97a75227643eeac61
humansinc1 <humans.inc@gmail.com>
2025-10-21 13:01:43 +0300

    added details to allure report

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -1,6 +1,11 @@
 import pytest
+import allure
 
 
+@allure.title('Test Empty Cart has correct text')
+@allure.epic('User journey')
+@allure.feature('Cart page')
+@allure.story('Empty Cart has correct text')
 def test_empty_cart_title_and_text_is_correct(cart_page):
     cart_page.open_page()
     cart_page.is_cart_title_correct()
@@ -8,6 +13,12 @@ def test_empty_cart_title_and_text_is_correct(cart_page):
 
 
 @pytest.mark.smoke
+@allure.suite('Smoke tests')
+@allure.severity('Blocker')
+@allure.title('Test Added product correctly displayed in Cart')
+@allure.epic('User journey')
+@allure.feature('Cart page')
+@allure.story('Added product is correctly displayed in cart')
 def test_product_added_to_cart_is_correct(cart_page, add_to_cart_pdp):
     cart_page.open_page()
     cart_page.is_cart_title_correct()
@@ -17,6 +28,12 @@ def test_product_added_to_cart_is_correct(cart_page, add_to_cart_pdp):
 
 
 @pytest.mark.smoke
+@allure.suite('Smoke tests')
+@allure.severity('Blocker')
+@allure.title('Test Checkout button redirects to Checkout page')
+@allure.epic('User journey')
+@allure.feature('Checkout')
+@allure.story('Clicking Checkout button in the cart redirects a user to Checkout page')
 def test_cart_checkout_button_redirects_to_checkout(cart_page, add_to_cart_pdp):
     cart_page.open_page()
     cart_page.is_checkout_button_clickable()
--- tests/test_product_details_page.py
+++ tests/test_product_details_page.py
@@ -1,19 +1,32 @@
+import allure
 import pytest
 
 
 @pytest.mark.smoke
+@allure.suite('Smoke tests')
+@allure.severity('Blocker')
+@allure.title('Test Add product to Cart from PDP')
+@allure.epic('User journey')
+@allure.feature('Add a product to cart')
+@allure.story('Add a product to cart from PDP')
 def test_add_product_to_cart(product_details_page):
     product_details_page.open_page()
     product_details_page.add_product_to_cart()
     product_details_page.was_product_added_to_the_cart()
 
-
+@allure.title('Test Add to cart button has correct color')
+@allure.epic('UI')
+@allure.feature('Cart')
+@allure.story('Add to cart button has correct color')
 def test_add_to_cart_button_has_correct_color(product_details_page):
     product_details_page.open_page()
     product_details_page.is_add_to_cart_button_color_correct()
     product_details_page.is_add_to_cart_button_hover_color_correct()
 
-
+@allure.title('Test Navigating through breadcrumbs')
+@allure.epic('User journey')
+@allure.feature('Navigation')
+@allure.story('Navigate through breadcrumbs')
 def test_breadcrumbs_are_clickable(product_details_page):
     product_details_page.open_page()
     product_details_page.are_breadcrumbs_clickable()
